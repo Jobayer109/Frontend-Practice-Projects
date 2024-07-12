0347eaf094295913b018f653af3d6db2c1bd2c44
user.email <jobayer.ahmed109@gmail.com>
2024-07-12 12:29:12 +0600

    map marked

--- Javascript/Mapty-App/script.js
+++ Javascript/Mapty-App/script.js
@@ -27,10 +27,10 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      });
     },
     function () {
       console.log("Can't access you location");
